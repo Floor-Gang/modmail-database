0a7ef070ef149926f05e57b2e03b5dfc72ffbbc6
Dylan Hackworth <dylhack@outlook.com>
2021-02-17 18:53:30 -0600

    Fixed parsing issue (2)

--- src/tables/threads.ts
+++ src/tables/threads.ts
@@ -28,7 +28,7 @@ export default class ThreadsTable extends Table {
       [userID],
     );
 
-    const threadIDs = res.rows;
+    const threadIDs = res.rows.map((th) => th.thread_id);;
     if (catID === null) {
       res = await this.pool.query(
         `SELECT * FROM modmail.threads WHERE id = ANY ($1) ORDER BY id`,
@@ -36,10 +36,7 @@ export default class ThreadsTable extends Table {
       );
     } else {
       res = await this.pool.query(
-        `SELECT * FROM modmail.threads
-           WHERE id = ANY ($1)
-           AND category = $2
-           ORDER BY id;`,
+        `SELECT * FROM modmail.threads WHERE id = ANY ($1) AND category = $2 ORDER BY id;`,
         [threadIDs, catID],
       );
     }
